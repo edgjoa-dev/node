03d29af4b618e57853e8f2ebb74d5d25051a0cb8
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2022-08-09 12:31:47 -0500

    Agregando carpetas routes y controllers

---
+++ controllers/user.js
@@ -0,0 +1,42 @@
+const { response } = require('express');
+
+
+const userGet = (req, res = response) => {
+
+    res.json({
+        message: 'Get - Controllers - User'
+    });
+}
+
+const putUser = (req, res = response) => {
+    res.status(400).json({
+        message: 'Put - Controllers - User'
+    });
+}
+const postUser = (req, res = response) => {
+
+    res.json({
+        message: 'Post - Controllers - User'
+    });
+}
+
+const patchUser = (req, res = response) => {
+    res.status(400).json({
+        message: 'Patch - Controllers - User'
+    });
+}
+const deleteUser = (req, res = response) => {
+    res.status(400).json({
+        message: 'Delete - Controllers - User'
+    });
+}
+
+
+
+module.exports = {
+    userGet,
+    putUser,
+    postUser,
+    patchUser,
+    deleteUser
+}
--- models/server.js
+++ models/server.js
@@ -5,6 +5,7 @@ class Server {
     constructor() {
         this.app = express();
         this.port = process.env.PORT;
+        this.pathUsers = '/api/users';
 
         //Middlewares
         this.middlewares();
@@ -25,36 +26,7 @@ class Server {
     }
 
     routes(){
-        this.app.get('/api', (req, res) => {
-            res.json({
-                ok: true,
-                message: ' Get'
-            });
-        })
-        this.app.put('/api', (req, res) => {
-            res.status(400).json({
-                ok: true,
-                message: ' Put'
-            });
-        })
-        this.app.post('/api', (req, res) => {
-            res.status(201).json({
-                ok: true,
-                message: ' Post'
-            });
-        })
-        this.app.patch('/api', (req, res) => {
-            res.json({
-                ok: true,
-                message: ' Patch'
-            });
-        })
-        this.app.delete('/api', (req, res) => {
-            res.json({
-                ok: true,
-                message: ' Delete'
-            });
-        })
+        this.app.use(this.pathUsers, require('../routes/user'));
     }
 
     listen(){
